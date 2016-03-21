1decf3fce587f89ce5deb16e892ae988502800e7
Andrew <andrew@substation.ru>
2016-03-21 11:02:16 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var config = require('config');
+var morgan = require('morgan');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(config.port);
