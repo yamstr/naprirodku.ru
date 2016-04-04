735bdf1142d5e2312e847cf4509a12dfdc0da246
Andrew <andrew@substation.ru>
2016-04-04 15:59:35 +0000

    Add strict routing

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var config = require('config');
-var morgan = require('morgan');
 var fs = require('fs');
+var config = require('config');
+var async = require('async');
+var validator = require('validator');
 var marked = require('marked');
 var express = require('express');
+var slash = require('express-slash');
+var morgan = require('morgan');
 var app = express();
 
 app.set('port', process.env.PORT || config.port);
 app.set('view engine', 'jade');
+app.enable('strict routing');
+app.use(slash());
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
@@ -14,13 +19,43 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/places/:id', function(req, res) {
-    fs.readFile(__dirname + '/data/plases/' + req.params.id + '/article.md', { encoding: 'utf8' }, function(err, data) {
-        if (err) throw err;
+app.get('/places/:id/', function(req, res) {
+    var path = __dirname + '/data/plases/' + req.params.id + '/index.md';
 
-        res.render('place', {
-            article: marked(data)
-        });
+    async.series([
+        function(callback) {
+            if (validator.isInt(req.params.id)) {
+                callback(null, true);
+            } else {
+                callback(true, false);
+            }
+        },
+        function(callback) {
+            fs.exists(path, function(exists) {
+                if (exists) {
+                    callback(null, true);
+                } else {
+                    callback(true, false);
+                }
+            });
+        },
+        function(callback) {
+            fs.readFile(path, { encoding: 'utf8' }, function(err, data) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, data);
+                }
+            });
+        }
+    ], function(err, results) {
+        if (err) {
+            res.status(404).end();
+        } else {
+            res.render('place', {
+                article: marked(results.pop())
+            });
+        }
     });
 });
 
